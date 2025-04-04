3084046f3c7396b5abdefcc61017b9fbfeb37033
kerleypablo <kerley.kls@gmail.com>
2025-04-04 15:15:29 -0300

    lint

--- src/pages/empresa/CadastroEmpresa.jsx
+++ src/pages/empresa/CadastroEmpresa.jsx
@@ -5,7 +5,7 @@ import { Button, Container, Form, Alert } from "react-bootstrap";
 import Header from "../../components/header/header";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; // Importação do CSS do Toastify
-import "./CadastroEmpresa.css";
+import "./cadastroEmpresa.css";
 
 function CadastroEmpresa() {
   const [empresaNome, setEmpresaNome] = useState("");
